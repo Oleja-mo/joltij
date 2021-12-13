141066ba4bcd234f173da9fe3a40a375ee11b8f5
Олег <oleg.mordovski@yandex.ru>
2021-12-13 08:36:03 +0500

    Заработало!

--- main.py
+++ main.py
@@ -4,35 +4,29 @@ import sys
 from PyQt5.QtGui import QPainter, QColor
 
 
-class Ui(QtWidgets.QDialog):
+class Main(QtWidgets.QDialog):
     def __init__(self):
-        super(Ui, self).__init__()
+        super(Main, self).__init__()
         uic.loadUi('UI.ui', self)
-        self.color = 'Yellow'
-        self.show()
         self.pushButton.clicked.connect(self.draw)
-        self.run = True
+        self.run = False
+        self.show()
 
     def draw(self):
         self.run = True
+        self.update()
 
     def paintEvent(self, event):
         if self.run:
             qp = QPainter()
             qp.begin(self)
-            self.drawing(qp)
+            qp.setBrush(QColor('Yellow'))
+            for i in range(10):
+                r = randint(10, 90)
+                qp.drawEllipse(randint(5, 300), randint(5, 300), r, r)
             qp.end()
-
-    def drawing(self, qp):
-        for i in range(randint(1, 10)):
-            qp.setBrush(QColor(self.color))
-            a = randint(1, 100)
-            qp.drawEllipse(10, 10, a, a)
-
-        self.update()
-        self.run = False
 
 
 app = QtWidgets.QApplication(sys.argv)
-window = Ui()
+window = Main()
 app.exec_()
